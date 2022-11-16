1bc408de039de93710a0d970475a88be5336ac63
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-11-16 09:38:22 +0300

    Done with assignment

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -4,8 +4,8 @@ class BakedGood < ActiveRecord::Base
   # This method could also be written using a scope:
   # https://guides.rubyonrails.org/active_record_querying.html#scopes
   # scope :by_price, -> { order(price: :desc) }
-  def self.by_price
-    self.all.order(price: :desc)
+  def BakedGood.by_price
+    BakedGood.all.order(price: :desc)
   end
 
 end
